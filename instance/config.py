3c4be7e46680bc4ad4c5f5bc6ac98022e2850dba
ss11mik <38486852+ss11mik@users.noreply.github.com>
2025-06-08 20:44:29 +0000

    Add files via upload

---
+++ instance/config.py
@@ -0,0 +1,13 @@
+CONFIG_FILE = "instance/config.ini"
+PASSWD_FILE = "instance/passwd"
+
+THEMES_FOLDER = '/opt/CPM/instance/themes/'
+TMP_FOLDER = '/opt/CPM/instance/tmp/'
+
+ALLOWED_EXTENSIONS = {'zip', '7z', 'rar', 'gz', 'bz'}
+
+GUI_ENABLED = True
+API_ENABLED = True
+
+AUTH_SERVER_IMPLEMENTATIONS = ["simple", "HTTP", "LLDAP", "FreeRADIUS"]
+PORTAL_IMPLEMENTATIONS = ["NDS", "pfSense"]
